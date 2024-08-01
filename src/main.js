de26f0b3fa3af18134964627651d6514062ede53
Daniel Santillan <daniel.santillan@eox.at>
2024-08-01 10:54:12 +0200

    chore: updating eodash dependency to latest release, updating config

--- src/main.js
+++ src/main.js
@@ -1,8 +1,4 @@
 import { createEodash } from "@eodash/eodash";
-import tools from "./tools";
-import basedOnWms from "./basedOnWms";
-import container from "./container";
-import backgroundMap from "./backgroundMap";
 
 export default createEodash((store) => ({
   id: "template-id",
@@ -31,30 +27,125 @@ export default createEodash((store) => ({
     }
   },
   template: {
-    gap: 6,
+    loading: {
+      id: Symbol(),
+      type: "web-component",
+      widget: {
+        // https://uiball.com/ldrs/
+        link: "https://cdn.jsdelivr.net/npm/ldrs/dist/auto/mirage.js",
+        tagName: "l-mirage",
+        properties: {
+          class: "align-self-center justify-self-center",
+          size: "120",
+          speed: "2.5",
+          color: "#004170",
+        },
+      },
+    },
     background: {
       id: Symbol(),
       type: "internal",
       widget: {
-        name: 'EodashMap'
-      }
+        name: "EodashMap",
+        properties: {
+          // enableCompare: true,
+        },
+      },
     },
     widgets: [
-      tools,
       {
         id: Symbol(),
         type: "internal",
-        title: "datepicker",
-        layout: { x: 5, y: 11, w: 2, h: 1 },
+        title: "Indicators",
+        layout: { x: 0, y: 0, w: 3, h: 8 },
         widget: {
-          name: 'EodashDatePicker',
-          props: {
-            inline: true,
+          name: "EodashItemFilter",
+          properties: {
+            // enableCompare: true,
+            aggregateResults: "themes",
           },
-        }
+        },
       },
-      basedOnWms(store.states),
-      // container(store.states)
+      {
+        id: Symbol(),
+        type: "internal",
+        title: "Layer Control",
+        layout: { x: 0, y: 8, w: 3, h: 4 },
+        widget: {
+          name: "EodashLayerControl",
+        },
+      },
+      {
+        defineWidget: (selectedSTAC) => {
+          return selectedSTAC
+            ? {
+                id: "Information",
+                title: "Information",
+                layout: { x: 9, y: 0, w: 3, h: 12 },
+                type: "web-component",
+                widget: {
+                  link: async () => await import("@eox/stacinfo"),
+                  properties: {
+                    for: store.states.currentUrl,
+                    allowHtml: "true",
+                    styleOverride: `.single-property {columns: 1!important;}
+                      h1 {margin:0px!important;font-size:16px!important;}
+                      header h1:after {
+                        content:' ';
+                        display:block;
+                        border:1px solid #d0d0d0;
+                      }
+                      h2 {font-size:15px}
+                      h3 {font-size:14px}
+                      summary {cursor: pointer;}
+                      #properties li > .value { font-weight: normal !important;}
+                      main {padding-bottom: 10px;}
+                      .footer-container {line-height:1;}
+                      .footer-container button {margin-top: -10px;}
+                      .footer-container small {font-size:10px;line-height:1;}`,
+                    header: '["title"]',
+                    tags: '["themes"]',
+                    subheader: "[]",
+                    properties: '["satellite","sensor","agency","extent"]',
+                    featured: '["description","providers","assets","links"]',
+                    footer: '["sci:citation"]',
+                  },
+                  tagName: "eox-stacinfo",
+                },
+              }
+            : null;
+        },
+      },
+      {
+        defineWidget: (selectedSTAC) => {
+          return selectedSTAC
+            ? {
+                id: "Datepicker",
+                type: "internal",
+                layout: { x: 5, y: 10, w: 1, h: 1 },
+                title: "Datepicker",
+                widget: {
+                  name: "EodashDatePicker",
+                },
+              }
+            : null;
+        },
+      },
+      {
+        defineWidget: (selected) => {
+          return selected
+            ? {
+                id: "Buttons",
+                layout: { x: 8, y: 0, w: 1, h: 1 },
+                title: "Buttons",
+                type: "internal",
+                widget: {
+                  name: "EodashMapBtns",
+                },
+              }
+            : null;
+        },
+      },
     ],
   },
 }));
