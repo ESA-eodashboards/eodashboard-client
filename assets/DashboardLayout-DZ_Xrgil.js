4cef0ffda81b000e70ab0a6630486295d69ba7c1
santilland <daniel.santillan@eox.at>
2024-08-01 14:57:42 +0000

    Deploying to gh-pages from @ ESA-eodash/eodashboard-client@3409f990984290790c6815923600fdfddccb9a7a 🚀

--- assets/DashboardLayout-DZ_Xrgil.js
+++ assets/DashboardLayout-DZ_Xrgil.js
@@ -1,4 +1,4 @@
-import{_ as m,x as y,W as x,X as d,$ as t,a0 as r,a1 as l,a9 as _,a3 as n,a6 as u,a2 as c,a4 as p,d as h,a5 as g,F as f,a7 as v,ab as w}from"./index-BtQV6Qx_.js";import{V as k}from"./VMain-BzNirA7Z.js";import"./ssrBoot-BDtwHRT-.js";class A extends HTMLElement{static get observedAttributes(){return["gap"]}constructor(){super(),this.attachShadow({mode:"open"}),this.render()}render(){this.shadowRoot.innerHTML=`
+import{_ as m,x as y,W as x,X as d,$ as t,a0 as r,a1 as l,a9 as _,a3 as n,a6 as u,a2 as c,a4 as p,d as h,a5 as g,F as f,a7 as v,ab as w}from"./index-BwCZ8lIL.js";import{V as k}from"./VMain-DtoG4t1Q.js";import"./ssrBoot--3QONwem.js";class A extends HTMLElement{static get observedAttributes(){return["gap"]}constructor(){super(),this.attachShadow({mode:"open"}),this.render()}render(){this.shadowRoot.innerHTML=`
     <style>
       :host {
         display: grid;
