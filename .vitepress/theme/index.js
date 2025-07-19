c1cf546f253a27b619182d9093785b134bb11be0
Lubomir <lubomir.dolezal@eox.at>
2025-07-19 20:00:24 +0200

    chore: update eodash version, increase width of processing and information panel by 1

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -12,6 +12,15 @@ export default {
       await import("@eox/storytelling");
       await import("@eox/layout");
       await import("@eox/itemfilter");
+      await import ("@eox/map");
+      await import ("@eox/chart");
+      await import ("@eox/drawtools");
+      await import ("@eox/jsonform");
+      await import ("@eox/stacinfo");
+      await import ("@eox/layercontrol");
+      await import ("color-legend-element");
+      await import ("@eox/timecontrol");
+      await import ("@eox/ui");
     }
   },
 };
