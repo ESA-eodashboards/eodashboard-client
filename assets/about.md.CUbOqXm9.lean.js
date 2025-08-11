b4539c957a0c3d08200a5bba849818ed250d7966
lubojr <lubojr@seznam.cz>
2025-08-11 15:25:02 +0000

    Deploying to gh-pages from @ ESA-eodashboards/eodashboard-client@c42b2889ad61def7732cddf6a535fc93e0d1782b 🚀

---
+++ assets/about.md.CUbOqXm9.lean.js
@@ -0,0 +1,1 @@
+import{_ as a,c as t,o,a3 as n}from"./chunks/framework.D-GfVUXH.js";const m=JSON.parse('{"title":"About","description":"","frontmatter":{},"headers":[],"relativePath":"about.md","filePath":"about.md"}'),s={name:"about.md"};function i(r,e,c,d,h,l){return o(),t("div",null,e[0]||(e[0]=[n("",11)]))}const u=a(s,[["render",i]]);export{m as __pageData,u as default};
