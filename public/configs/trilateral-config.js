07f88b162da497dadb52af6951d016a7788d497c
Lubomir <lubomir.dolezal@eox.at>
2026-01-15 18:24:18 +0100

    fix: datepicker style for normal screensizes

--- public/configs/trilateral-config.js
+++ public/configs/trilateral-config.js
@@ -169,7 +169,7 @@ export default {
               ? {
                   id: "expert-datepicker",
                   type: "internal",
-                  layout: { x: 4, y: 7, w: 4, h: 5 },
+                  layout: { x: 4, y: 6, w: 4, h: 6 },
                   title: "Date",
                   widget: {
                     name: "EodashDatePicker",
@@ -337,7 +337,7 @@ export default {
               ? {
                   id: "DatePicker",
                   type: "internal",
-                  layout: { x: 4, y: 7, w: 4, h: 5 },
+                  layout: { x: 4, y: 6, w: 4, h: 6 },
                   title: "Date",
                   widget: {
                     name: "EodashDatePicker",
@@ -479,7 +479,7 @@ export default {
               ? {
                   id: "DatePicker",
                   type: "internal",
-                  layout: { x: 4, y: 7, w: 4, h: 5 },
+                  layout: { x: 4, y: 6, w: 4, h: 6 },
                   title: "Date",
                   widget: {
                     name: "EodashDatePicker",
@@ -670,7 +670,7 @@ export default {
               ? {
                   id: "DatePickerCompare",
                   type: "internal",
-                  layout: { x: 4, y: 7, w: 4, h: 5 },
+                  layout: { x: 4, y: 6, w: 4, h: 6 },
                   title: "Date",
                   widget: {
                     name: "EodashDatePicker",
