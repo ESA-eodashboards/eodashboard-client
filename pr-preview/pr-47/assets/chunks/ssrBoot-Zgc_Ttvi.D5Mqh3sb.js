dcf61aac508ccf43641e4887a95d0ed27e66fbc0
lubojr <lubojr@users.noreply.github.com>
2026-04-16 08:21:24 +0000

    Deploy preview for PR 47 🛫

---
+++ pr-preview/pr-47/assets/chunks/ssrBoot-Zgc_Ttvi.D5Mqh3sb.js
@@ -0,0 +1,1 @@
+import{v as t,S as e,G as s,au as n}from"./framework.DxuH9_e6.js";function i(){const o=s(!1);return t(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:n(()=>o.value?void 0:{transition:"none !important"}),isBooted:e(o)}}export{i as u};
