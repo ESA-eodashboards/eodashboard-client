81a4068fb9cbdf8d48399d675a92714f241cf170
Lubomir Dolezal <lubojr@seznam.cz>
2026-07-23 14:30:02 +0200

    feat: group newsletter, workspace and resources into "Ecosystem"

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -38,15 +38,24 @@ export default defineConfig({
       position: "bottom-right",
     },
     nav: [
-      { text: "Home", link: "/" },
       { text: "News & Updates", link: "/news_updates" },
-      { text: "Resources", link: "/resources" },
-      { text: "Dashboard", link: "/explore" },
       {
-        text: "Newsletter",
-        link: "https://assets-eur.mkt.dynamics.com/846bcca8-9413-4d04-822b-7282239af232/digitalassets/standaloneforms/1596564b-a729-f011-8c4d-7c1e52350f4e",
-        target: "_blank",
+        text: 'Ecosystem',
+        items: [
+          { text: "Resources", link: "/resources" },
+          {
+            text: "Newsletter",
+            link: "https://assets-eur.mkt.dynamics.com/846bcca8-9413-4d04-822b-7282239af232/digitalassets/standaloneforms/1596564b-a729-f011-8c4d-7c1e52350f4e",
+            target: "_blank",
+          },
+          {
+            text: "Workspace",
+            link: "https://workspace.eodashboard.hub-otc.eox.at",
+            target: "_blank",
+          },
+        ]
       },
+      { text: "Dashboard", link: "/explore" },
       { text: "About", link: "/about" },
     ],
   },
